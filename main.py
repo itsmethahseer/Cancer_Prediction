56bca8a9efbe852821665abc604a93872353c09f
Thahseer <zacthahseer123@gmail.com>
2023-07-25 20:58:17 +0530

    Model deployed using fastapi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import joblib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,28 +7,30 @@ from pydantic import BaseModel
 import uvicorn
 # Load the saved model
 model = joblib.load('cancer_model.pkl')
-
+import sklearn
+print(sklearn.__version__)
 # Define the input data schema
 class PredictionInput(BaseModel):
-    GENDER: str
-    AGE: int
-    SMOKING: str
-    YELLOW_FINGERS: str
-    ANXIETY: str
-    PEER_PRESSURE: str
-    CHRONIC_DISEASE: str
-    FATIGUE: str
-    ALLERGY: str
-    WHEEZING: str
-    ALCOHOL_CONSUMING: str
-    COUGHING: str
-    SHORTNESS_OF_BREATH: str
-    SWALLOWING_DIFFICULTY: str
-    CHEST_PAIN: str
-
+    gender: str = Form()
+    age: int = Form()
+    smoking: str = Form()
+    yellowFingers: str = Form()
+    anxiety: str = Form()
+    peerPressure: str = Form()
+    chronicDisease: str = Form()
+    fatigue: str = Form()
+    allergy: str = Form()
+    wheezing: str = Form()
+    alcoholConsuming: str = Form()
+    coughing: str = Form()
+    shortnessOfBreath: str = Form()
+    swallowingDifficulty: str = Form()
+    chestPain: str = Form()
+    
+    
 # Create the FastAPI application
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="static")
 
 # Mount the static files directory to serve the HTML and CSS files
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -37,13 +39,49 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Define the prediction route
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("/index.html", {"request": request})
 
 
 @app.post("/predict")
-async def predict_lung_cancer(data: PredictionInput):
+async def predict_lung_cancer(
+    request: Request,
+    gender: str = Form(),
+    age: int = Form(),
+    smoking: str = Form(),
+    yellowFingers: str = Form(),
+    anxiety: str = Form(),
+    peerPressure: str = Form(),
+    chronicDisease: str = Form(),
+    fatigue: str = Form(),
+    allergy: str = Form(),
+    wheezing: str = Form(),
+    alcoholConsuming: str = Form(),
+    coughing: str = Form(),
+    shortnessOfBreath: str = Form(),
+    swallowingDifficulty: str = Form(),
+    chestPain: str = Form()
+):
     # Convert the input data to a dictionary
-    input_data = data.dict()
+    
+    input_data = {
+        # "gender":gender,
+        # "age":float(age),
+        # "smoking":float(smoking),
+        "yellowFingers":float(yellowFingers),
+        "anxiety":float(anxiety),
+        "peerPressure":float(peerPressure),
+        "chronicDisease":float(chronicDisease),
+        "fatigue":float(fatigue),
+        "allergy":float(allergy),
+        "wheezing":float(wheezing),
+        "alcoholConsuming":float(alcoholConsuming),
+        "coughing":float(coughing),
+        "shortnessOfBreath":float(shortnessOfBreath),
+        "swallowingDifficulty":float(swallowingDifficulty),
+        "chestPain":float(chestPain)
+    }
+    
+    print(type(input_data))
     
     # Prepare the input features for prediction
     input_features = [input_data[col] for col in input_data]
@@ -51,7 +89,9 @@ async def predict_lung_cancer(data: PredictionInput):
     # Make the prediction
     prediction = model.predict([input_features])[0]
     
+
+    
     # Return the prediction as a JSON response
-    return {"prediction": prediction}
+    return {"prediction": int(prediction)}
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
